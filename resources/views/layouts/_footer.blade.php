da65c437fc9061978ba07259c1c42832424c744a
colbertwong <colbertwong@outlook.com>
2018-08-29 07:18:43 +0000

    Add css etc.

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,14 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img src="https://lccdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34" class="brand-icon">
+                <a href="https://laravel-china.org/courses">good good study, day day up!</a>
+            </img>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="/about">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
